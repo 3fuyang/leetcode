f1a756400229577654131afd9e6e4d7f557d610e
fwio <624835339@qq.com>
2025-04-30 10:06:51 +0800

    feat(416): space optimization

--- src/problems/416-partition-equal-subset-sum/index.ts
+++ src/problems/416-partition-equal-subset-sum/index.ts
@@ -5,22 +5,23 @@ export function canPartition(nums: number[]): boolean {
     return false
   }
 
-  const volume = sum / 2
-  const dp: boolean[][] = Array.from({ length: nums.length }, () =>
-    new Array(volume + 1).fill(false),
-  )
-  dp[0][0] = true
+  const target = sum / 2
+  // one-dimensional dp array for optimization on space
+  // here j means capacity:
+  //   dp[j] = dp[j] || dp[j - nums[i]]
+  const dp: boolean[] = new Array(target + 1).fill(false)
+  // initialize dp array
+  dp[0] = true
 
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j <= volume; j++) {
-      if (nums[i] > j) {
-        dp[i][j] = dp[i - 1][j]
-        continue
+  // traverse the item(number)s from right to left
+  for (const num of nums) {
+    for (let j = target; j > 0; j--) {
+      // guard
+      if (j >= num) {
+        dp[j] = dp[j] || dp[j - num]
       }
-
-      dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]
     }
   }
 
-  return dp[nums.length - 1][volume]
+  return dp[target]
 }
