5da0cea136bdddfa9c5b089487479b7d8ecde5d3
fwio <624835339@qq.com>
2025-08-05 23:22:35 +0800

    feat: problem 160

---
+++ src/problems/160-intersection-of-two-linked-lists/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(
+	headA: ListNode | null,
+	headB: ListNode | null,
+): ListNode | null {
+	let ptrA = headA;
+	let ptrB = headB;
+
+	while (ptrA !== ptrB) {
+		ptrA = ptrA ? ptrA.next : headB;
+		ptrB = ptrB ? ptrB.next : headA;
+	}
+
+	return ptrA;
+}
+
+class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
